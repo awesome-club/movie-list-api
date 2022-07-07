db94cfbfb4bbc835aa0e125c15923e6f0b912175
Ionut Velicu <velicu.ionut@gmail.com>
2022-07-07 21:54:22 +0300

    In memory db

--- env.ts
+++ env.ts
@@ -1,4 +1,4 @@
 export const MovieDbKey = "2d9822ec42a661e9b83cbc5b7caab3cf";
 export const MovieDbPath = "https://api.themoviedb.org/3";
 export const DatabaseName = "movie-list.db";
-export const InMemoryDb = false;
+export const InMemoryDb = true;
