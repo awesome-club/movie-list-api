97451a6585f73c928f6592acf8485819b59f7ba0
Ionut Velicu <velicu.ionut@gmail.com>
2022-07-10 22:49:40 +0300

    Fix

--- handlers/setup.handler.ts
+++ handlers/setup.handler.ts
@@ -3,7 +3,7 @@ import { uuid } from "../services/id.service.ts";
 import * as ListService from "../services/list.service.ts";
 
 export function init({ request, response }: Context) {
-  const id = request.headers.get("X-MID") ?? uuid();
+  const id = request.headers.get("X-MID") || uuid();
   const lists = id ? ListService.getForMember(id) : [];
   response.body = { id, lists };
 }
